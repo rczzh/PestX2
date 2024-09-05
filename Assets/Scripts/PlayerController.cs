028db6e6870a51518a32bf2901113b47e3d0da32
rczzh <ryanchng.0808@gmail.com>
2024-09-05 20:04:16 +0800

    Added collection system

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    new Rigidbody2D rigidbody;
+    private Vector2 moveDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    void ProcessInput()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        moveDirection = new Vector2(horizontal, vertical).normalized;
+    }
+
+    void Move()
+    {
+        rigidbody.velocity = new Vector2(moveDirection.x * speed, moveDirection.y * speed);
+    }
+}
